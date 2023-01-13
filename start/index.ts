2655da5b541c389913c78f79b553f5b0e97f6707
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2023-01-13 11:44:54 +0100

    Update

--- start/index.ts
+++ start/index.ts
@@ -1,5 +1,6 @@
-export const handler = async (event: any) => {
+export const handler = async (event) => {
   const body = JSON.parse(event.body);
+
   return {
     statusCode: 200,
     body: JSON.stringify(!body.name ? 'Hi there!' : `Hi ${body.name}!`)
